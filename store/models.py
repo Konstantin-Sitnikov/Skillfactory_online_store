0cbd8add1893a3746ec2b2c43da23514f415e050
Konstantin-Sitnikov <robokot_90@mail.ru>
2025-02-03 13:09:12 +0900

    next

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from email.policy import default
 from random import choice
+from datetime import datetime, timezone
 
 from django.db import models
 
@@ -44,3 +45,58 @@ class Staff(models.Model):
     full_name = models.CharField(max_length=255)
     position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
     labor_contract = models.IntegerField(default=0)
+
+    def get_last_name(self):
+        return self.full_name.split()[0]
+
+
+# CREATE TABLE ORDERS(
+#     order_id INT AUTO_INCREMENT NOT NULL,
+#     time_in DATETIME NOT NULL,
+#     time_out DATETIME,
+#     cost FLOAT NOT NULL,
+#     pickup INT NOT NULL,
+#     staff INT NOT NULL,
+#     PRIMARY KEY(order_id),
+#     FOREIGN KEY(staff) REFERENCES STAFF(staff_id)
+# );
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default = 0.0)
+    pickup = models.BooleanField(default = False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through = 'ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:  # если завершён, возвращаем разность объектов
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:  # если ещё нет, то сколько длится выполнение
+            return (datetime.now(timezone.utc) - self.time_in).total_seconds() // 60
+
+# CREATE TABLE PRODUCTS_ORDERS(
+#     product_order_id INT AUTO_INCREMENT NOT NULL,
+#     product INT NOT NULL,
+#     in_order INT NOT NULL,
+#     amount INT NOT NULL,
+#     PRIMARY KEY(product_order_id),
+#     FOREIGN KEY(product) REFERENCES PRODUCTS(product_id),
+#     FOREIGN KEY(in_order) REFERENCES ORDERS(order_id)
+# );
+
+class ProductOrder(models.Model):
+    amount = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    in_order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
+
+
